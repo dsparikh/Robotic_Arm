181c4ef36551c0d7b5812acb67d45fd08803afeb
Dhrumilkumar Parikh <dsparikh@uwaterloo.ca>
2018-01-21 00:41:55 -0500

    Cleaner Code

--- Robotic_Arm.ino
+++ Robotic_Arm.ino
@@ -6,31 +6,43 @@
 
 #include <Servo.h>
 
-#define L1                      235.0
-#define L2                      130.0
-#define L3                      130
+#define L1                          235.0
+#define L2                          130.0
+#define L3                          130
 
-#define BASE_SERVO				0
-#define SHOULDER_SERVO			1
-#define ELBOW_SERVO				2
-#define WRIST_SERVO				3
-#define WRIST_ROTATE_SERVO	 	4
-#define GRIPPER_SERVO			5
+#define BASE_SERVO                  0
+#define SHOULDER_SERVO              1
+#define ELBOW_SERVO                 2
+#define WRIST_SERVO                 3
+#define WRIST_ROTATE_SERVO          4
+#define GRIPPER_SERVO               5
+#define RIGHT_PLATFORM              6
+#define LEFT_PLATFORM               7
 
-#define GRIPPER_CLOSED			100
-#define GRIPPER_OPENED			45
+
+#define RIGHT_PLATFORM_MOTOR_STOP   78
+#define LEFT_PLATFORM_MOTOR_STOP    88
+
+#define GRIPPER_CLOSED              100
+#define GRIPPER_OPENED              45
 
 /*Servos are numbered bottom to top (on arm)*/
-Servo servo[6];			  // Create 6 servos
+Servo servo[8];         // Create 8 servos
 
-//servo[0]			      // BASE			Servo 0 Bottom most 
-//servo[1]		          // SHOULDER		Servo 1  
-//servo[2]		          // ELBOW			Servo 2
-//servo[3]		          // WRIST			Servo 3
-//servo[4]		          // WRIST ROTATE	Servo 4
-//servo[5]		          // GRIPPER		Servo 5 Top Most
+						//servo[0]              // BASE     Servo 0 Bottom most 
+						//servo[1]              // SHOULDER   Servo 1  
+						//servo[2]              // ELBOW      Servo 2
+						//servo[3]              // WRIST      Servo 3
+						//servo[4]              // WRIST ROTATE Servo 4
+						//servo[5]              // GRIPPER    Servo 5 Top Most
+						//servo[6]              // Right Platform Servo
+						//servo[7]              // Left Platfrom Servo
 
-void setup() 
+#define PLATFORM_DIRECTION_RIGHT	0
+#define PLATFORM_DIRECTION_LEFT		1
+
+
+void setup()
 {
 	Serial.begin(9600);
 	Serial.println("Arduino Running...");
@@ -40,7 +52,11 @@ void setup()
 	servo[WRIST_SERVO].attach(5);
 	servo[WRIST_ROTATE_SERVO].attach(6);
 	servo[GRIPPER_SERVO].attach(7);
+	servo[RIGHT_PLATFORM].attach(8);
+	servo[LEFT_PLATFORM].attach(9);
 }
+
+int PlatfromCounter = 0;
 
 void arm_home()
 {
@@ -48,133 +64,122 @@ void arm_home()
 	servo[SHOULDER_SERVO].write(100);
 	servo[ELBOW_SERVO].write(100);
 	servo[WRIST_SERVO].write(85);
-	servo[WRIST_ROTATE_SERVO].write(90);
+	servo[WRIST_ROTATE_SERVO].write(93);
 	servo[GRIPPER_SERVO].write(GRIPPER_CLOSED);
-	delay(1000);
+	servo[RIGHT_PLATFORM].write(93);
+	servo[LEFT_PLATFORM].write(92);
+	//delay(1000);
 }
-
-int poser = 0; // initial position of servero
-void ControlWithKeyBoard()
-{
-	int val; // initial value of input
-
-	if (Serial.available()) // if serial value is available 
-	{
-		val = Serial.read();// then read the serial value
-		if (val == 'd') //if value input is equals to d
-		{
-			poser += 10; //than position of servo motor increases by 1 ( anti clockwise)
-			servo[WRIST_SERVO].write(poser);// the servo will move according to position
-			delay(15);//delay for the servo to get to the position
-		}
-		if (val == 'a') //if value input is equals to a
-		{
-			poser -= 10; //than position of servo motor decreases by 1 (clockwise)
-			servo[WRIST_SERVO].write(poser);// the servo will move according to position
-			delay(15);//delay for the servo to get to the position
-		}
-
-	}
-	Serial.println(poser);
-	if (poser >= 180)
-	{
-		poser = 180;
-	}
-	if (poser <= 0)
-	{
-		poser = 0;
-	}
-}
-
 
 void loop()
 {
-	////arm_home();
-	//Smooth(0, 100, 200,  200, 100, 200);// Step 1 -> Step 2
-	////Goto(0, 100, 200); // Step 1
-	////delay(2000);
-	////Goto(200, 100, 200);// Step 2
-	////delay(2000);
-	//Smooth(200, 100, 200,  200, 200, 200);// Step 2 -> Step 3
-	////Goto(200, 200, 200);// Step 3
-	//Grab();
-	//Smooth(200, 200, 200,  200, 100, 200);// Step 3 -> Step 4
-	////delay(2000);
-	////Goto(200, 100, 200);// Step 4
-	////delay(2000);
-	//Smooth(200, 100, 200,  -50, 100, 200);// Step 4 -> Step 5
-	////Goto(-50, 100, 200);// Step 5
-	////delay(2000);
-	//Smooth(-50, 100, 200,  -100, 100, 200);// Step 5 -> Step 6
-	////Goto(-100, 100, 200); //(0,0,0) Step 6
-	////delay(2000);
-	//Smooth(-100, 100, 200,  -150, 100, 0);// Step 6 -> Step 7
-	////Goto(-150, 100, 0);// Step 7
-	////delay(2000);
-	//Smooth(-150, 100, 0,  -100, 100, 0);// Step 7 -> Step 8
-	////Goto(-100, 100, 0);// Step 8
-	////delay(2000);
-	//Smooth(-100, 100, 0,  -100, 100,150); //Step 8 -> Step 9
-	//delay(10000);
-	//Smooth(-100, 100, 150,  -100, 100, 0);//Step 10 -> Step 11
-	//
-	//Smooth(-100, 100, 0,  -150, 100, 0);//Step 11 -> Step 12
-	////Goto(-150, 100, 0);// Step 12
-	////delay(2000);
-	//Smooth(-150, 100, 0,  -50, 100, 200);//Step 12 -> Step 13
-	////Goto(-50, 100, 200);// Step 13
-	////delay(2000);
-	//Smooth(-50, 100, 200,  -100, 100, 200);//Step 13 -> Step 14
-	////Goto(-100, 100, 200); //(0,0,0) Step 14
-	////delay(2000);
-	//Smooth(-100, 100, 200,  -50, 100, 200);//Step 14 -> Step 15
-	////Goto(-50, 100, 200); //Step 15
-	////delay(2000);
-	//Smooth(-50, 100, 200,  200, 100, 200);//Step 15 -> Step 19
-	////Goto(200, 100, 200); // Step 19
-	////delay(2000);
-	//Smooth(200, 100, 200,  0, 100, 200);//Step 10 -> Step 20
-	//
-	////Goto(0, 100, 200); // Step 20
-	////delay(5000);
-	//servo[GRIPPER_SERVO].write(95);
-    //delay(5000);
-    //servo[GRIPPER_SERVO].write(180);
-    //delay(5000);
-	Goto(0, 100, 62);// Step 8
-  servo[WRIST_ROTATE_SERVO].write(180);
-  servo[GRIPPER_SERVO].write(60);
-  delay(1000);
-  servo[GRIPPER_SERVO].write(85);
-  delay(1000);
-  servo[WRIST_ROTATE_SERVO].write(45);
-  delay(1000);
-  servo[GRIPPER_SERVO].write(60);
-  delay(1000);
-  servo[WRIST_ROTATE_SERVO].write(180);
-  delay(1000);
+	int home_pos[3] = { 0, 155, 190 };
+
+	arm_home();
+	delay(500);
+
+/*****************************************Slider*******************************************************/
+
+	int slider_pos[3] = {-20, 190, 75};
+
+	servo[GRIPPER_SERVO].write(GRIPPER_OPENED);
+	Smooth(home_pos[0], home_pos[1], home_pos[2] , slider_pos[0], slider_pos[1], slider_pos[2]);
+	delay(500);
+	servo[GRIPPER_SERVO].write(GRIPPER_CLOSED);
+	Smooth(slider_pos[0], slider_pos[1], slider_pos[2], 180, slider_pos[1], slider_pos[2]);
+	delay(2000);
+	Smooth(180, slider_pos[1], slider_pos[2], -50, slider_pos[1], slider_pos[2]);
+	servo[GRIPPER_SERVO].write(GRIPPER_OPENED);
+	delay(500);
+	Smooth(-50, slider_pos[1], slider_pos[2], -50, home_pos[1], home_pos[2]);
+	Smooth(-50, home_pos[1], home_pos[2], home_pos[0], home_pos[1], home_pos[2]);
+
+/****************************************Pipet 1*******************************************************/
+
+	int pipet1_pos[3] = {-250, 190, 40};
+	int cup_pos[3] = {150, 170, 250};
+
+	servo[GRIPPER_SERVO].write(65);
+	Smooth(home_pos[0], home_pos[1], home_pos[2], pipet1_pos[0], pipet1_pos[1], pipet1_pos[2]);
+	servo[GRIPPER_SERVO].write(GRIPPER_CLOSED);
+	Smooth(pipet1_pos[0], pipet1_pos[1], pipet1_pos[2], pipet1_pos[0], pipet1_pos[1], 200);
+	Smooth(pipet1_pos[0], pipet1_pos[1], 200, cup_pos[0], cup_pos[1], cup_pos[2]);
+	 
+	Smooth(cup_pos[0], cup_pos[1], cup_pos[2], cup_pos[0], cup_pos[1], 150);
+	servo[GRIPPER_SERVO].write(85);
+	delay(1000);
+	Smooth(cup_pos[0], cup_pos[1], 150, cup_pos[0], cup_pos[1], cup_pos[2]);
+	Smooth(cup_pos[0], cup_pos[1], cup_pos[2], home_pos[0], home_pos[1], home_pos[2]);
+
+/****************************************Test Tube*******************************************************/
+	//MovePlatform(PLATFORM_DIRECTION_RIGHT, 5000);
 
 
+	//  Smooth(200,170,200,0,155,190);
+	//  delay(1000);
+	//  servo[RIGHT_PLATFORM].write(77+10);
+	//  servo[LEFT_PLATFORM].write(88+10);
+	//  delay(6000);
+	//  servo[RIGHT_PLATFORM].write(77);
+	//  servo[LEFT_PLATFORM].write(88);
+	//  Smooth(0,155,190,-165,0,100);
+	//  servo[GRIPPER_SERVO].write(100);
+	//  delay(5000);
+	//  Smooth(-165,0,100,-165,0,200);
+	//  
+	//  servo[RIGHT_PLATFORM].write(77-10);
+	//  servo[LEFT_PLATFORM].write(88-10);
+	//  delay(5800);
+	//  servo[RIGHT_PLATFORM].write(77);
+	//  servo[LEFT_PLATFORM].write(88);
+	//  //garbage
+	//  Smooth(-165,0,200,200,80,50);
+	//  servo[GRIPPER_SERVO].write(65);
 
+/****************************************Cap Closing*******************************************************/
+	//servo[GRIPPER_SERVO].write(60);
+	//delay(500);
+	//Smooth(0,155,190,-120,150,5);
+	//servo[GRIPPER_SERVO].write(80);
+	//Smooth(-120,150,5,-120,150,100);
+	//Smooth(-120,150,100,0,155,190);
+
+	//servo[RIGHT_PLATFORM].write(77+10);
+	//servo[LEFT_PLATFORM].write(88+10);
+	//delay(6000);
+	//servo[RIGHT_PLATFORM].write(77);
+	//servo[LEFT_PLATFORM].write(88);
+	//Smooth(0,155,190,-130,20,60);
+	//delay(5000);
+	////Close Cap
+	//Smooth(-130,20,60,-130,20,100);
+	//servo[RIGHT_PLATFORM].write(77-10);
+	//servo[LEFT_PLATFORM].write(88-10);
+	//delay(5000);
+	//servo[RIGHT_PLATFORM].write(77);
+	//servo[LEFT_PLATFORM].write(88);
+
+/****************************************End*******************************************************/
+	delay(2000000);
 }
 
 //From -> To
-#define TIME	10
+#define TIME  5
 void Smooth(int x, int y, int z, int x2, int y2, int z2)
 {
 	delay(1000);
 	//x
-	if (x < x2) 
+	if (x < x2)
 	{
 		for (x; x < x2; x++)
 		{
 			Goto(x, y, z); delay(TIME);
 		}
 	}
-	else 
+	else
 	{
-		for (x; x > x2 ; x--)
-		{ 
+		for (x; x > x2; x--)
+		{
 			Goto(x, y, z); delay(TIME);
 		}
 	}
@@ -209,22 +214,6 @@ void Smooth(int x, int y, int z, int x2, int y2, int z2)
 		}
 	}
 	delay(1000);
-
-
-
-
-
-	//for (int i = 0; i <= 150; i += 1)
-	//{
-	//	Goto(-100, 100, i);// Step 9
-	//	delay(50);
-	//}
-	//delay(10000);//Step 10
-	//for (int i = 150; i >= 0; i -= 1)
-	//{
-	//	Goto(-100, 100, i);// Step 11
-	//	delay(50);
-	//}
 }
 
 void Grab()
@@ -234,6 +223,28 @@ void Grab()
 	servo[GRIPPER_SERVO].write(GRIPPER_CLOSED);
 }
 
+void MovePlatform(int direction, int time)
+{
+	switch (direction)
+	{
+	case PLATFORM_DIRECTION_RIGHT:
+		servo[RIGHT_PLATFORM].write(93 + 10);
+		servo[LEFT_PLATFORM].write(92 + 10);
+		delay(time);
+		servo[RIGHT_PLATFORM].write(93);
+		servo[LEFT_PLATFORM].write(92);
+		break;
+	case PLATFORM_DIRECTION_LEFT:
+		servo[RIGHT_PLATFORM].write(93 - 10);
+		servo[LEFT_PLATFORM].write(92 - 10);
+		delay(time);
+		servo[RIGHT_PLATFORM].write(93);
+		servo[LEFT_PLATFORM].write(92);
+		break;
+	default:
+		break;
+	}
+}
 
 /*Two link Inverse kinametics*/
 void Goto(float x, float y, float z)
@@ -257,8 +268,8 @@ void Goto(float x, float y, float z)
 	if (t3 < 0) { t3 = abs(t3); }
 
 	servo[BASE_SERVO].write(t1);
-	servo[SHOULDER_SERVO].write(180-t2);
+	servo[SHOULDER_SERVO].write(180 - t2);
 	servo[ELBOW_SERVO].write(97);//Always constent
-	servo[WRIST_SERVO].write(190-t3);
+	servo[WRIST_SERVO].write(190 - t3);
 
 }
